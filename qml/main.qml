7edb354c98a3dd47038fbbb8bab78befb7c834ba
Hans Ole Hatzel <hhatzel@gmail.com>
2018-06-22 10:43:19 +0200

    Auto import from same directory not working

---
+++ qml/main.qml
@@ -0,0 +1,12 @@
+import QtQuick 2.11
+import QtQuick.Controls 1.4
+
+ApplicationWindow {
+    visible: true
+
+    CustomComponent {}
+
+    Text {
+        text: "test"
+    }
+}
---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include <QQmlApplicationEngine>
+#include <QApplication>
+
+int main(int argc, char** argv) {
+    QApplication app(argc, argv);
+    QQmlApplicationEngine engine;
+    engine.load(QUrl("qrc:main.qml"));
+    app.exec();
+}
