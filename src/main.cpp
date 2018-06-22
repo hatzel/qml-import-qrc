55e468f6f8b51fae0957ea35cc14e7c451f89209
Hans Ole Hatzel <hhatzel@gmail.com>
2018-06-22 11:24:13 +0200

    Fix qml importing

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,6 @@
 int main(int argc, char** argv) {
     QApplication app(argc, argv);
     QQmlApplicationEngine engine;
-    engine.load(QUrl("qrc:main.qml"));
+    engine.load(QUrl("qrc:/main.qml"));
     app.exec();
 }
